3f8d881f3b8ada59f9d5f040390dcd1e2072b9fb
Stanislaw Makarchuk <49611600+StasMakarchuk@users.noreply.github.com>
2024-10-07 10:57:41 +0100

    Create conf_truing.py

---
+++ conf_truing.py
@@ -0,0 +1,19 @@
+path_adata: /nfs/team283_imaging/playground_Stas/Embryo/0424_flatten_map_new/wEMBsp13458120/wEMBsp13458120.h5ad
+name: section1
+x_lim: [5000, 20000]
+y_lim: [50000, 80000]
+pos_a: [8000, 65000]
+vector_a: [200, 200]
+pos_b: [9000, 60000]
+vector_b: [200, 200]
+out_folder: /nfs/team283_imaging/playground_Stas/Embryo/0424_flatten_map_new/wEMBsp13458120/out_sections
+rotate_90: False
+U_shape: True
+save_images: True
+shorten_median_prc: 25
+subcrop_x: 'None'
+subcrop_y: 'None'
+downscale: 400
+method: 'default'
+rotate_angle: -90
+rotate_c_point: [12500, 65000]
